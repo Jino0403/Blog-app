ec5bb6a547cb29c9e9362b5de4820bfd826103d1
Jino0403 <jinho9203@naver.com>
2024-02-26 22:42:30 +0900

    docs: 블로그앱 만들어보기 기본구조완

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <Link to="/">글쓰기</Link>
+      </header>
+      <div></div>
+      <footer></footer>
+    </>
+  );
+}
