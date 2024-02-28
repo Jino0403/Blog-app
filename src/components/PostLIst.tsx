afb02d56daa629ac1c4acce2edcbe9a17fb4386f
Jino0403 <jinho9203@naver.com>
2024-02-28 20:23:32 +0900

    feat: firestore 게시글 삭제구현

--- src/components/PostLIst.tsx
+++ src/components/PostLIst.tsx
@@ -1,8 +1,9 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
 import { db } from 'firebaseApp';
 import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from './context/AuthContext';
+import { toast } from 'react-toastify';
 
 interface PostListProps {
   hasNavigation?: boolean;
@@ -28,13 +29,22 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
 
   const getPosts = async () => {
     const datas = await getDocs(collection(db, 'posts'));
-
-    console.log(datas);
+    setPosts([]);
+    // posts 초기화를 해주지 않으면 삭제시 새로고침 전까지 데이터가 여러개 추가되어있음.
 
     datas?.forEach((doc) => {
       const dataObj = { ...doc.data(), id: doc.id };
       setPosts((prev) => [...prev, dataObj as PostProps]);
     });
+  };
+
+  const handleDelete = async (id: string) => {
+    const confirm = window.confirm('게시물을 삭제하시겠습니까?');
+    if (confirm && id) {
+      await deleteDoc(doc(db, 'posts', id));
+      toast.success('게시물을 삭제하였습니다.');
+      getPosts();
+    }
   };
 
   useEffect(() => {
@@ -75,7 +85,12 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
               </Link>
               {post?.email === user?.email && (
                 <div className="post__utils-box">
-                  <div className="post__delete">삭제 </div>
+                  <div
+                    className="post__delete"
+                    role="presentation"
+                    onClick={() => handleDelete(post.id as string)}>
+                    삭제
+                  </div>
                   <div className="post__edit">
                     <Link to={`/posts/edit/${post?.id}`}>수정</Link>
                   </div>
